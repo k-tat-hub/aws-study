5fb9c22a2b3c16d160d5e46024e614c5638f7654
Tatsuya Kanno <283382228+k-tat-hub@users.noreply.github.com>
2026-05-20 18:54:34 +0900

    refactor: outputの名前を修正

--- terraform/lesson33/environments/dev/outputs.tf
+++ terraform/lesson33/environments/dev/outputs.tf
@@ -1,14 +1,14 @@
-output "ALBDnsName" {
+output "alb_dns_name" {
   value       = module.network.alb_dns_name
   description = "DNS name of the ALB"
 }
 
-output "EC2PublicIP" {
+output "ec2_public_ip" {
   value       = module.compute.ec2_public_ip
   description = "Public IP of the EC2"
 }
 
-output "RDSEndpoint" {
+output "rds_endpoint" {
   value       = module.database.rds_endpoint
   description = "Endpoint of the RDS"
 }
