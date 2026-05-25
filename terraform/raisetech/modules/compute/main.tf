f288fe9359697af83c7e256021c376600025b995
Tatsuya Kanno <283382228+k-tat-hub@users.noreply.github.com>
2026-05-25 20:56:17 +0900

    EC2を2台構成に変更

--- terraform/raisetech/modules/compute/main.tf
+++ terraform/raisetech/modules/compute/main.tf
@@ -62,17 +62,17 @@ resource "aws_instance" "ec2" {
   }
 }
 
-# resource "aws_instance" "ec2_2" {
-#   ami                         = var.ec2_ami
-#   instance_type               = var.ec2_instance_type
-#   subnet_id                   = var.subnet_public_a_id
-#   associate_public_ip_address = true
-#   vpc_security_group_ids = [
-#     aws_security_group.ec2.id
-#   ]
-#   key_name = var.ec2_keypair
+resource "aws_instance" "ec2_2" {
+  ami                         = var.ec2_ami
+  instance_type               = var.ec2_instance_type
+  subnet_id                   = var.subnet_public_a_id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [
+    aws_security_group.ec2.id
+  ]
+  key_name = var.ec2_keypair
 
-#   tags = {
-#     Name = "raisetech-ec2_2"
-#   }
-# }
+  tags = {
+    Name = "raisetech-ec2-2"
+  }
+}
