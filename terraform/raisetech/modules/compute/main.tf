7d86b4d569516b9a71e3561682ace04f36136135
Tatsuya Kanno <283382228+k-tat-hub@users.noreply.github.com>
2026-05-25 21:19:54 +0900

    環境差し戻し

--- terraform/raisetech/modules/compute/main.tf
+++ terraform/raisetech/modules/compute/main.tf
@@ -61,18 +61,3 @@ resource "aws_instance" "ec2" {
     Name = "raisetech-ec2"
   }
 }
-
-resource "aws_instance" "ec2_2" {
-  ami                         = var.ec2_ami
-  instance_type               = var.ec2_instance_type
-  subnet_id                   = var.subnet_public_a_id
-  associate_public_ip_address = true
-  vpc_security_group_ids = [
-    aws_security_group.ec2.id
-  ]
-  key_name = var.ec2_keypair
-
-  tags = {
-    Name = "raisetech-ec2-2"
-  }
-}
